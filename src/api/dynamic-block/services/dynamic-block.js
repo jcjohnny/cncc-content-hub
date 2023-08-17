500505019fc1cf022fc5feb20a7baf9a6e990ffa
aratner <alexander_ratner@condenast.com>
2023-08-17 14:48:14 -0400

    updated copilot image preview

---
+++ src/api/dynamic-block/services/dynamic-block.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dynamic-block service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::dynamic-block.dynamic-block');
